e00b77f9bc2d5aab016e3e859e343886713b7652
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-09-16 14:11:24 +0530

    Create withdrawals.js

---
+++ api/withdrawals.js
@@ -0,0 +1,18 @@
+import dbConnect from "./_db.js";
+import User from "../models/User.js";
+
+export default async function handler(req, res) {
+  await dbConnect();
+
+  if (req.method !== "GET") return res.status(405).json({ error: "Method not allowed" });
+
+  try {
+    const { userId } = req.query;
+    const user = await User.findOne({ userId });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.status(200).json({ withdrawals: user.withdrawals });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
